2cd755b9a8328760f02238e91ab4164532cdeb8d
Rodrigo Monsalve <kayj.tune@gmail.com>
2013-11-16 01:04:04 -0300

    Conexión entre Jugadores y servidor de partidas

--- Servidor_Partidas.js
+++ Servidor_Partidas.js
@@ -1,24 +1,64 @@
-const net = require("net");
-var contadorLocal = 0;
+var puerto = 8007;//puerto por el cual se conectaran al servidor de nodejs html
+var io= require('socket.io').listen(puerto, { log: false });
+console.log("Se ha iniciado el servidor de partidas");
 
-var socket = new net.Socket();
-socket.connect(61337, "127.0.0.1", function () {//Se conecta a la IP "127.0.0.1" por el puerto "61337"
-    console.log("Conexión establecida");
+const net = require("net");
+
+var usuariosConectados = new Array();
+var idCentralUsuariosConectados = new Array();
+
+var socketNet = new net.Socket();
+socketNet.connect(61337, "127.0.0.1", function () {//Se conecta al servidor central a través de la IP "127.0.0.1" por el puerto "61337"
+  console.log("Conexión establecida");
 });
 
-socket.on('data', function(data){//Se reciben todos los mensajes enviados desde el servidor central en la variable "data"
+////// Funciones de comunicación entre el servidor y clientes///////////////
+
+io.sockets.on('connection', function (socket) {
+
+	console.log ("se ha conectado el usuario " + socket.id);
+	usuariosConectados.push(socket.id);
+		
+	});
+
+
+
+//////////////////////////
+
+
+socketNet.on('data', function(data){//Se reciben todos los mensajes enviados desde el servidor central en la variable "data"
 	data = JSON.parse(data);//Se decodifica mensaje data para convertirlo en un objeto JS
 	switch(data.opcion){
 		case 0: // Cuando se manda un mensaje para que este servidor gestione una partida
 		 		console.log ("Me encargare de gestionar la partida entre " + data.jugador1 + " y " + data.jugador2);
+		 		idCentralUsuariosConectados.push(data.jugador1);
+		 		idCentralUsuariosConectados.push(data.jugador2);
+		 		//while (  ( (usuariosConectados.indexOf(data.jugador1) == -1) ||  (usuariosConectados.indexOf(data.jugador2) == -1) )) {}
+		 		console.log(usuariosConectados.indexOf(data.jugador1));
+		 		//comenzarPartida(data.jugador1,data.jugador2);
 		break;
 
-		case 1:
-	  			if(data.contadorLocal%10==0)console.log("contador global: "+data.contadorGlobal+" contador local: "+data.contadorLocal);
-	  	break;
 	}
 });
 
-socket.on('error', function (err) {
+socketNet.on('siguienteTurno',function(datos){
+
+	io.sockets.socket(datos.oponente).emit('miTurno',{});
+
+});
+
+socketNet.on('error', function (err) {
   console.log('error'+ err);
 });
+
+function comenzarPartida(jugador1,jugador2){
+
+	console.log("Va a comenzar la partida entre " + jugador1+ " y " + jugador2);
+	io.sockets.socket(jugador1).emit('comenzarPartida1',{oponente: jugador2});
+	io.sockets.socket(jugador2).emit('comenzarPartida2',{oponente: jugador1});
+
+}
+
+
+
+
